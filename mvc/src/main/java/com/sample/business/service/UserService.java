00348f37933d937be15197a0291c3de76dc9bcab
o_youcan_o <o_youcan_o@yahoo.co.jp>
2020-12-28 22:57:29 +0900

    lombok適用

--- mvc/src/main/java/com/sample/business/service/UserService.java
+++ mvc/src/main/java/com/sample/business/service/UserService.java
@@ -2,22 +2,17 @@ package com.sample.business.service;
 import com.sample.business.model.User;
 import com.sample.dao.UserDao;
 
+import lombok.Data;
 
+@Data
 //本当はインターフェースからimplemetsすべきですが、ここでは簡略のためそのままクラスを作っています。
 public class UserService {
 	private UserDao userDao;
-	
-	public UserDao getUserDao() {
-	return userDao;
-	}
-	public void setUserDao(UserDao userDao) {
-		this.userDao = userDao;
-	}
-	
+
 	public User getUser(String id){
 		return this.userDao.getUser(id);
 	}
-	
+
 	public void updateUser(User user){
 		this.userDao.updateUser(user);
 	}
